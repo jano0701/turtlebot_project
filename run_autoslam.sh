72e23d72fb8f815d14b32cd3eeaf5129df9a713c
FerasAbushahla24 <ferasshahla2016@gmail.com>
2025-06-04 19:12:01 +0200

    Update run_autoslam.sh mit Navigation und Prozess-Handling

--- run_autoslam.sh
+++ run_autoslam.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "🗑  Alte Karten gelöscht."
+rm -f ~/catkin_ws/src/slam_exploration_bot/map/*.pgm
+rm -f ~/catkin_ws/src/slam_exploration_bot/map/*.yaml
+
 # Automatischer Start: Gazebo + Navigation + Exploration
 
 export TURTLEBOT3_MODEL=burger
@@ -10,7 +14,6 @@ echo "🚀 Starte Gazebo (Simulation)..."
 gnome-terminal --tab --title="Gazebo" -- bash -c "
     source /opt/ros/noetic/setup.bash;
     source ~/catkin_ws/devel/setup.bash;
-    export TURTLEBOT3_MODEL=burger;
     roslaunch turtlebot3_gazebo turtlebot3_world.launch;
     exec bash
 "
@@ -49,17 +52,31 @@ while [ ! -f ~/catkin_ws/src/slam_exploration_bot/map/map.yaml ]; do
   sleep 1
 done
 
-echo "🧭 Karte erkannt. Starte Navigation..."
+echo "🗺️ Karte erkannt. Starte Navigation..."
 
+echo "🛑 Beende alle Prozesse..."
 # === Prozesse beenden ===
 rosnode kill /turtlebot3_slam_gmapping
+rosnode kill /pure_pursuit
+rosnode kill /rviz
+rosnode kill /frontier_exploration
+
+sleep 8
+
+killall -q -9 rviz
+pkill -f pure_pursuit.py
+pkill -f frontier_exploration
+
+echo "🧭 Starte Navigation mit geladener Karte..."
+
+sleep 10
 
 # 4. Starte Navigation mit geladener Karte
 gnome-terminal --tab --title="Navigation" -- bash -c "
     source /opt/ros/noetic/setup.bash;
     source ~/catkin_ws/devel/setup.bash;
     export TURTLEBOT3_MODEL=burger;
-    roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:=~/catkin_ws/src/slam_exploration_bot/map/map.yaml; 
+    roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:=$HOME/catkin_ws/src/slam_exploration_bot/map/map.yaml; 
     exec bash
 "
 
